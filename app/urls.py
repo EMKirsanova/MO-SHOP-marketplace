3583484d9c25288e9a9eaf10e8bceb5167d3372f
Vlad Loparev <loparev.official@yandex.ru>
2025-05-11 03:36:11 +0500

    Goods app is added, catalog.html and product.html are connected

--- app/urls.py
+++ app/urls.py
@@ -19,5 +19,18 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls', namespace='main'))
+    path('', include('main.urls', namespace='main')),
+    path('catalog/', include('goods.urls', namespace='catalog'))
 ]
+
+"""
+www.site.com/admin
+
+www.site.com/
+www.site.com/about
+www.site.com/contacts
+www.site.com/delivery_and_payments
+
+www.site.com/catalog
+www.site.com/catalog/product
+"""
