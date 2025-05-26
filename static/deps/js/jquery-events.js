c9338fd0afa9aeeca9888c3657ba02308b1b80d4
Vlad Loparev <loparev.official@yandex.ru>
2025-05-26 18:20:38 +0500

    Add-to-cart ajax request & json response

--- static/deps/js/jquery-events.js
+++ static/deps/js/jquery-events.js
@@ -1,14 +1,5 @@
 // Когда html документ готов (прорисован)
 $(document).ready(function () {
-    // Берем из разметки элемент по id - оповещения от django
-    var notification = $('#notification');
-    // И через 7 сек. убираем
-    if (notification.length > 0) {
-        setTimeout(function () {
-            notification.fadeOut();
-        }, 7000);
-    }
-
 // // При клике по значку корзины открываем всплывающее(модальное) окно
 // $('#modalButton').click(function () {
 //     $('#exampleModal').appendTo('body');
