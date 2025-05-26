379a59b60df4be74dc8cc1fb7bce205249edb773
Vlad Loparev <loparev.official@yandex.ru>
2025-05-27 00:09:39 +0500

    Admin panel setup

--- goods/models.py
+++ goods/models.py
@@ -1,6 +1,7 @@
 from statistics import quantiles
 from tabnanny import verbose
 from django.db import models
+from django.urls import reverse
 
 
 # Create your models here.
@@ -48,6 +49,9 @@ class Products(models.Model):
     def __str__(self):
         return f'{self.name} | Количество: {self.quantity}'
 
+    def get_absolute_url(self):
+        return reverse("catalog:product", kwargs={"product_slug": self.slug})
+    
     def display_id(self):
         return f'{self.id:05}' # 00007
     
--- users/utils.py
+++
@@ -1,2 +0,0 @@
-def join_carts(request):
-    
